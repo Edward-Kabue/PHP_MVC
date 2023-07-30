0abd17723fdbf44ac8964350a8833b454bc2a98e
Edward-Kabue <ekabue0@gmail.com>
2023-07-30 20:07:02 +0300

    Add docstring to the router class

--- framework/Routing/Route.php
+++ framework/Routing/Route.php
@@ -10,6 +10,13 @@ class Route
     protected array $parameters = [];
     protected ?string $name = null;
 
+    /**
+     * Constructs a new instance of the class.
+     *
+     * @param string $method The HTTP method.
+     * @param string $path The path of the route.
+     * @param callable $handler The callback function that handles the route.
+     */
     public function __construct(string $method, string $path, callable $handler)
     {
         $this->method = $method;
@@ -17,21 +24,43 @@ class Route
         $this->handler = $handler;
     }
 
+    /**
+     * Retrieves the method.
+     *
+     * @param string $method The method to retrieve.
+     * @return string The retrieved method.
+     */
     public function method(string $method): string
     {
         return $this->method;
     }
 
+    /**
+     * Get the path of the object.
+     *
+     * @return string The path of the object.
+     */
     public function path()
     {
         return $this->path;
     }
 
+    /**
+     * Returns the parameters of the object.
+     *
+     * @return array The parameters of the object.
+     */
     public function parameters(): array
     {
         return $this->parameters;
     }
 
+    /**
+     * Sets or retrieves the name of the object.
+     *
+     * @param string $name The name to set for the object. If null, the function will retrieve the current name.
+     * @return string|null The name of the object if $name is null, otherwise returns null.
+     */
     public function name(string $name = null)
     {
         if ($name) {
@@ -42,6 +71,13 @@ class Route
         return $this->name;
     }
 
+    /**
+     * Checks if the given method and path match the current object's method and path.
+     *
+     * @param string $method The method to compare.
+     * @param string $path The path to compare.
+     * @return bool Returns true if the method and path match, false otherwise.
+     */
     public function matches(string $method, string $path): bool
     {
         if (
@@ -97,6 +133,13 @@ class Route
         return false;
     }
 
+    /**
+     * Normalizes the given path by removing leading and trailing slashes,
+     * adding a leading slash, and removing consecutive slashes.
+     *
+     * @param string $path The path to be normalized.
+     * @return string The normalized path.
+     */
     private function normalisePath(string $path): string
     {
         $path = trim($path, '/');
@@ -106,6 +149,13 @@ class Route
         return $path;
     }
 
+    /**
+     * Dispatches the function.
+     *
+     * @throws Some_Exception_Class description of exception
+     * @return Some_Return_Value
+     */
+  
     public function dispatch()
     {
         return call_user_func($this->handler);
